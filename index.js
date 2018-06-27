43aed01bbf9ad6ce822192d9f42bbf267c50ad82
unknown <polloli.hugo@gmail.com>
2018-06-27 19:08:35 +0200

    chore: add .gitignore and index.js, install ethers

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const ethers = require('ethers');
+const express = require('express');
+let app = express();
+
+
+app.listen(3000, (err) => {
+	if(err)
+        console.log(err);	
+});
